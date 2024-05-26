5d19c0af51aa152562e5c62ef3de578a8ab564dd
Sapphire645 <167467641+Sapphire645@users.noreply.github.com>
2024-05-26 14:05:38 +0200

    Add files via upload

--- src/Models/Tile.cs
+++ src/Models/Tile.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace DQB2ChunkEditor.Models;
@@ -8,8 +9,17 @@ public class Tile
     [JsonPropertyName("Type")]
     public Type Type { get; set; } = Type.Block;
 
+    [JsonPropertyName("ListId")]
+    public short ListId => (short)(Id % 2048);
+
+    [JsonPropertyName("Is Overflowed")]
+    public bool Overflow => Id > 2047;
+
     [JsonPropertyName("Id")]
-    public short Id { get; set; } = 0;
+    public short Id { get; set; } = -1;
+
+    [JsonPropertyName("ImageId")]
+    public short ImageId { get; set; } = -1;
 
     [JsonPropertyName("Name")]
     public string Name { get; set; } = "Unknown";
@@ -17,6 +27,18 @@ public class Tile
     [JsonPropertyName("Description")]
     public string Description { get; set; } = "N/A";
 
+    [JsonPropertyName("Can be broken by")]
+    public string Break { get; set; } = "N/A";
+
+    [JsonPropertyName("Used")]
+    public string Used { get; set; } = "N/A";
+
+    [JsonPropertyName("Color")]
+    public bool inList { get; set;} = false;
+
+    [JsonPropertyName("Map")]
+    public string ImagMap => $"/Images/Tiles/{ImageId:0000}.png";
+
     [JsonIgnore]
-    public string Image => $"/Images/Blocks/{Id:0000}.png";
+    public string Image => $"/Images/Blocks/{ImageId:0000}.png";
 }
