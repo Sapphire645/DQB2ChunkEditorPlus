e5923435600a9f0ab937fbec172bfee1ac941d4f
Sapphire645 <167467641+Sapphire645@users.noreply.github.com>
2024-07-03 00:15:23 +0200

    Add files via upload
    Added options to change Gratitude points, time, and weather [not fully tested]

--- src/ChunkEditor.cs
+++ src/ChunkEditor.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Reflection.Emit;
 using System.Security.Cryptography.Xml;
 using System.Text;
+using System.Windows.Markup;
+using System.Buffers.Binary;
 using Ionic.Zlib;
 
 namespace DQB2ChunkEditor;
@@ -22,6 +24,11 @@ public static class ChunkEditor
     public static string Filename { get; set; } = null!;
     public const uint LayerHeight = 0x60; // layers are up to 96 blocks high
     public static short ChunkCount { get; set; } = 0; // chunk count changes per map
+    public static short ItemCount { get; set; } = 0; // chunk count changes per map
+
+    public static UInt32 GratitudePoints{ get; set; } = 99999; //gratitude points
+    public static float Clock { get; set; } = 0; //clock
+    public static ushort Weather{ get; set; } = 1; //weather
 
     public static void LoadFile(string filename)
     {
@@ -47,7 +54,26 @@ public static class ChunkEditor
         chunkBytes[0] = _uncompressedBytes[1331903-HeaderLength];
         chunkBytes[1] = _uncompressedBytes[1331904-HeaderLength];
         ChunkCount = (short)(BitConverter.ToUInt16(chunkBytes));
+
+        chunkBytes[0] = _uncompressedBytes[0x24E7CD];
+        chunkBytes[1] = _uncompressedBytes[0x24E7CE];
+        ItemCount = (short)(BitConverter.ToUInt16(chunkBytes));
         Filename = filename;
+        //extra
+        var GratitudeBytes = new byte[4];
+        GratitudeBytes[0] = _uncompressedBytes[0xC0FDC-HeaderLength];
+        GratitudeBytes[1] = _uncompressedBytes[0xC0FDD-HeaderLength];
+        GratitudeBytes[2] = _uncompressedBytes[0xC0FDE-HeaderLength];
+        GratitudeBytes[3] = _uncompressedBytes[0xC0FDF-HeaderLength];
+        GratitudePoints = BitConverter.ToUInt32(GratitudeBytes);
+
+        GratitudeBytes[0] = _uncompressedBytes[0xC1060-HeaderLength];
+        GratitudeBytes[1] = _uncompressedBytes[0xC1061-HeaderLength];
+        GratitudeBytes[2] = _uncompressedBytes[0xC1062-HeaderLength];
+        GratitudeBytes[3] = _uncompressedBytes[0xC1063-HeaderLength];
+        Clock = BitConverter.ToSingle(GratitudeBytes);
+
+        Weather = _uncompressedBytes[0xC1064-HeaderLength];
     }
 
     public static void SaveFile()
@@ -97,16 +123,41 @@ public static class ChunkEditor
         ChunkCount = (short)(BitConverter.ToUInt16(chunkBytes));
 
         Filename = filename;
+        //extra
+        var GratitudeBytes = new byte[4];
+        GratitudeBytes[0] = _uncompressedBytes[0xC0FDC-HeaderLength];
+        GratitudeBytes[1] = _uncompressedBytes[0xC0FDD-HeaderLength];
+        GratitudeBytes[2] = _uncompressedBytes[0xC0FDE-HeaderLength];
+        GratitudeBytes[3] = _uncompressedBytes[0xC0FDF-HeaderLength];
+        GratitudePoints = BitConverter.ToUInt32(chunkBytes);
+        
+        GratitudeBytes[0] = _uncompressedBytes[0xC1060-HeaderLength];
+        GratitudeBytes[1] = _uncompressedBytes[0xC1061-HeaderLength];
+        GratitudeBytes[2] = _uncompressedBytes[0xC1062-HeaderLength];
+        GratitudeBytes[3] = _uncompressedBytes[0xC1063-HeaderLength];
+        Clock = BitConverter.ToSingle(chunkBytes);
+
+        Weather = _uncompressedBytes[0xC1064-HeaderLength];
     }
     private static uint GetByteIndex(short chunk, byte layer, short tile)
     {
         return (uint)(BlockStart + (chunk * ChunkSize) + (layer * LayerSize) + (tile * 2));
     }
 
-    //private static uint GetItemIndex(short chunk, byte layer, short tile)
-    //{
-    //    return (uint)(BlockStart + (chunk * ChunkSize) + (layer * LayerSize) + (tile * 2));
-    //}
+    public static uint GetItemData(uint address)
+    {
+        return _uncompressedBytes[address];
+    }
+    
+
+    public static uint GetItemChunk(uint address)
+    {
+        var blockBytes = new byte[2];
+        blockBytes[0] = _uncompressedBytes[address];
+        blockBytes[1] = _uncompressedBytes[address + 1];
+
+        return BitConverter.ToUInt16(blockBytes);
+    }
     public static ushort GetBlockValue(short chunk, byte layer, short tile)
     {
         var index = GetByteIndex(chunk, layer, tile);
@@ -201,29 +252,20 @@ public static class ChunkEditor
                     SetBlockValue(chunk, layer, tile, 0);
             
             chunk++;
-            }
-                
+        }
     }
-
-    //code by Turtle_insect
-
-    // public static void GetItemValue(short chunk, byte layer, short tile, short blockId)
-    // {
-    //     var address = GetItemIndex(short chunk, byte layer, short tile);
-
-    //     ItemID = SaveData.Instance().ReadNumber(address + 8, 1);
-    //     var tmp = SaveData.Instance().ReadNumber(address + 9, 1) & 0x1F; //mask
-    //     ItemID += tmp * 256;
-
-    //     PosX = SaveData.Instance().ReadNumber(address + 9, 1) >> 5;
-    //     tmp = SaveData.Instance().ReadNumber(address + 10, 1) & 0x3; //mask
-    //     PosX += tmp << 3;
-
-    //     PosY = SaveData.Instance().ReadNumber(address + 10, 1) >> 2;
-    //     tmp = SaveData.Instance().ReadNumber(address + 11, 1) & 0x1; //mask
-    //     PosY += tmp << 6;
-
-    //     PosZ = (SaveData.Instance().ReadNumber(address + 11, 1) & 0x3E);
-    //     PosZ >>= 1;
-    // }
+    public static void UpdateExtra(){
+        byte[] GratBytes = new byte[4];
+        BinaryPrimitives.WriteUInt32LittleEndian(GratBytes,GratitudePoints);
+        _uncompressedBytes[0xC0FDC-HeaderLength] = GratBytes[0];
+        _uncompressedBytes[0xC0FDD-HeaderLength] = GratBytes[1];
+        _uncompressedBytes[0xC0FDE-HeaderLength] = GratBytes[2];
+        _uncompressedBytes[0xC0FDF-HeaderLength] = GratBytes[3];
+        BinaryPrimitives.WriteSingleLittleEndian(GratBytes,Clock);
+        _uncompressedBytes[0xC1060-HeaderLength] = GratBytes[0];
+        _uncompressedBytes[0xC1061-HeaderLength] = GratBytes[1];
+        _uncompressedBytes[0xC1062-HeaderLength] = GratBytes[2];
+        _uncompressedBytes[0xC1063-HeaderLength] = GratBytes[3];
+        _uncompressedBytes[0xC1064-HeaderLength] = (byte)Weather;
+    }
 }
